142a4a0376290a885f17fb26a825dc4f144a0626
Arnav Gupta <arnav@codingblocks.com>
2019-09-08 17:08:24 +0530

    js basics
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Lecture06/jsbasics/myfile.js
@@ -0,0 +1,21 @@
+let x = 10
+let y = 'kjabikjibdf'
+let z = false
+let a = [1, 2, 3, 4]
+
+console.log(typeof x)
+
+console.log(typeof typeof x)
+
+let o = {
+  a: 10,
+  b: 20,
+  c: true,
+  d: false,
+  e: 'sfssfh',
+  f: [2, 456, 573, 3],
+  g: {
+    k: 10,
+    l: 'adgag'
+  }
+}
