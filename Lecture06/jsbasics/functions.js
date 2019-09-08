1fd1a563f0c4427520483bc398bf48cc86028482
Arnav Gupta <arnav@codingblocks.com>
2019-09-08 17:15:33 +0530

    strict mode example
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture06/jsbasics/functions.js
+++ Lecture06/jsbasics/functions.js
@@ -1,5 +1,15 @@
+"use strict";
+
 function sum(a, b) {
-  if (b == undefined)
+  if (b == undefined) {
     b = 0
+  }
+
   return a + b
 }
+
+for (i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+x = 10
