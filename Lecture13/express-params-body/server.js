543f806ba91888b443740fa546421a9100579f19
Arnav Gupta <arnav@codingblocks.com>
2019-10-12 18:30:01 +0530

    sending bodies
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture13/express-params-body/server.js
+++ Lecture13/express-params-body/server.js
@@ -1,5 +1,25 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    'server.log',
+    `
+    ==============
+    URL    = ${req.url}
+    QUERY  = ${JSON.stringify(req.query)}
+    PARAMS = ${JSON.stringify(req.params)}
+    ===============
+
+  `,
+    () => {},
+  )
+  next()
+})
 
 app.get('/hello', (req, res) => {
   if (!req.query.name) req.query.name = 'Guest'
@@ -13,6 +33,11 @@ app.get('/bye/:name', (req, res) => {
   }
 
   res.send(req.query.greeting + ' ' + req.params.name)
+})
+
+app.post('/xyz', (req, res) => {
+  console.log(req.body)
+  res.send('Done')
 })
 
 app.listen(7676)
