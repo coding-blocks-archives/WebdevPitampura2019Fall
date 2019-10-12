9055efe6081367e5ead4adf3105e92fa1c8d7423
Arnav Gupta <arnav@codingblocks.com>
2019-10-12 16:49:00 +0530

    sort promise
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture11/3-file-sort/sort-p.js
+++ Lecture11/3-file-sort/sort-p.js
@@ -1,18 +1,22 @@
-const fs = require('fs').promises
+const fs = require('fs')
+const util = require('util')
 const os = require('os')
 os.EOL // instead of '\n'
 
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+
 Promise.all([
-  fs.readFile(__dirname + '/1.txt'),
-  fs.readFile(__dirname + '/2.txt'),
-  fs.readFile(__dirname + '/3.txt')
+  readFile(__dirname + '/1.txt'),
+  readFile(__dirname + '/2.txt'),
+  readFile(__dirname + '/3.txt')
 ]).then((arr) => {
   let nums = []
   arr.forEach((fileData) => {
     nums = nums.concat(fileData.toString().split(os.EOL))
   })
   nums = nums.sort((a, b) => a - b)
-  return fs.writeFile(__dirname + '/out2.txt', nums)
+  return writeFile(__dirname + '/out2.txt', nums)
 }).then(() => {
   console.log('All done!')
-})
+}).catch()
