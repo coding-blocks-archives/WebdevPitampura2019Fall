6f2403949ec45ad8051909b5dd75851867e64774
Arnav Gupta <arnav@codingblocks.com>
2019-11-10 16:55:29 +0530

    creating articles
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture17/blog-project/routes/pages/index.js
+++ Lecture17/blog-project/routes/pages/index.js
@@ -1,19 +1,35 @@
 const { Router } = require('express')
-const { getAllArticles } = require('../../controllers/articles')
+const { getAllArticles, createArticle } = require('../../controllers/articles')
+const { getAllUsers } = require('../../controllers/users')
 
 const route = Router()
 
 route.get('/', async (req, res) => {
 
   const articles = await getAllArticles()
+  const users = await getAllUsers()
+
   res.render('home', {
     title: 'Home',
-    articles,
+    articles, users
   })
+})
+
+route.post('/articles', async (req, res) => {
+  try {
+    const article = await createArticle(req.body.author_id, req.body.title, req.body.subtitle, req.body.body)
+    return res.redirect('/')
+  } catch (e) {
+    return res.status(500).send({
+      error: e,
+    })
+  }
 })
 
 route.get('/article', (req, res) => {
   res.render('article', { title: 'Article' })
 })
+
+route.use('/users', require('./users'))
 
 module.exports = route
