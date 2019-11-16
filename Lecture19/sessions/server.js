3f4470684f3ce1c03cec5000e449e420e477acfa
Arnav Gupta <arnav@codingblocks.com>
2019-11-16 18:37:57 +0530

    session management using cookies
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture19/sessions/server.js
+++ Lecture19/sessions/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const session = require('express-session')
 const { db, Users } = require('./db/models')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'jh4btk34hbyo452by3k5jb',
+  resave: false,
+  saveUninitialized: false,
+}))
 
 app.get('/signup', (req, res) => {
   res.render('signup')
@@ -36,7 +43,20 @@ app.post('/login', async (req, res) => {
   if (!user) return res.redirect('/login')
 
   if (user.password === req.body.password) {
+    req.session.userid = user.id
     res.redirect('/profile')
+  } else {
+    res.redirect('/login')
+  }
+})
+
+app.get('/profile', async (req, res) => {
+  if (req.session.userid) {
+    const user = await Users.findOne({
+      where: {id: req.session.userid}
+    })
+
+    res.render('profile', { user })
   } else {
     res.redirect('/login')
   }
